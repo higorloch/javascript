1743d4d008bef3487047a52ca90bc505ca85bc97
higorloch <higor.kotarski@escola.pr.gov.br>
2026-06-16 12:59:28 +0000

    .

---
+++ main.js
@@ -0,0 +1,8 @@
+<script src="main.js"></script>
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+console.log(botoes)
